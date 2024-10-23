a40f11c91e76d47b5cff16387c0562d7ef6d1891
benboguan <nanchuci023@gmail.com>
2024-10-23 14:03:38 +0800

    mediatek: fix 7981rxxx

--- target/linux/mediatek/image/mt7981.mk
+++ target/linux/mediatek/image/mt7981.mk
@@ -84,13 +84,13 @@ define Device/mt7981-spim-nand-7981r128
 endef
 TARGET_DEVICES += mt7981-spim-nand-7981r128
 
-define Device/mt7981-spim-nand-7981r128-gsw
+define Device/mt7981-spim-nand-7981r128-g
   DEVICE_VENDOR := MediaTek
-  DEVICE_MODEL := mt7981-spim-nand-7981r128-gsw
-  DEVICE_DTS := mt7981-spim-nand-7981r128-gsw
+  DEVICE_MODEL := mt7981-spim-nand-7981r128-g
+  DEVICE_DTS := mt7981-spim-nand-7981r128-g
   DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
   DEVICE_PACKAGES := $(MT7981_USB_PKGS)
-  SUPPORTED_DEVICES := mediatek,mt7981-spim-snand-7981r128
+  SUPPORTED_DEVICES := mediatek,mt7981-spim-snand-7981r128-g
   UBINIZE_OPTS := -E 5
   BLOCKSIZE := 128k
   PAGESIZE := 2048
@@ -100,15 +100,15 @@ define Device/mt7981-spim-nand-7981r128-gsw
   IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
-TARGET_DEVICES += mt7981-spim-nand-7981r128-gsw
+TARGET_DEVICES += mt7981-spim-nand-7981r128-g
 
-define Device/mt7981-spim-nand-7981r128-wan-p5-gsw
+define Device/mt7981-spim-nand-7981r128-wan-p5-g
   DEVICE_VENDOR := MediaTek
-  DEVICE_MODEL := mt7981-spim-nand-7981r128-wan-p5-gsw
-  DEVICE_DTS := mt7981-spim-nand-7981r128-wan-p5-gsw
+  DEVICE_MODEL := mt7981-spim-nand-7981r128-wan-p5-g
+  DEVICE_DTS := mt7981-spim-nand-7981r128-wan-p5-g
   DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
   DEVICE_PACKAGES := $(MT7981_USB_PKGS)
-  SUPPORTED_DEVICES := mediatek,mt7981-spim-snand-7981r128
+  SUPPORTED_DEVICES := mediatek,mt7981-spim-snand-7981r128-wan-p5-g
   UBINIZE_OPTS := -E 5
   BLOCKSIZE := 128k
   PAGESIZE := 2048
@@ -118,7 +118,7 @@ define Device/mt7981-spim-nand-7981r128-wan-p5-gsw
   IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
-TARGET_DEVICES += mt7981-spim-nand-7981r128-wan-p5-gsw
+TARGET_DEVICES += mt7981-spim-nand-7981r128-wan-p5-g
 
 define Device/mt7981-spim-nand-sfp
   DEVICE_VENDOR := MediaTek
