fe234248798bd3d92e0e731452bdabd7173d0d76
benboguan <nanchuci023@gmail.com>
2025-07-27 00:38:40 +0800

    luci-app-wifi-mtk: add MLREnable support

--- package/mtk/applications/luci-app-wifi-mtk/root/usr/share/luci-app-wifi-mtk/wireless-mtk.js
+++ package/mtk/applications/luci-app-wifi-mtk/root/usr/share/luci-app-wifi-mtk/wireless-mtk.js
@@ -1012,6 +1012,9 @@ return view.extend({
 					o = ss.taboption('advanced', form.Flag, 'whnat', _('Wireless HWNAT'));
 					o.default = o.enabled;
 
+					o = ss.taboption('advanced', form.Flag, 'mlr', _('Wireless MLR'));
+					o.default = o.disabled;
+
 					o = ss.taboption('advanced', form.Flag, 'bandsteering', _('Band Steering'));
 					o.default = o.disabled;
 
